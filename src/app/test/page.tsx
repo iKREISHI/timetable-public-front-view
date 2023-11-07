deda8943872e865013e09b042bd1a8121c8d8740
shiro-av <vadas25@yandex.ru>
2023-11-07 11:37:28 +0500

    некоторый функционал

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -12,7 +12,7 @@ const getWeekDates = (inputDate: Date): Date[] => {
     const currentDay = new Date(startOfWeek);
     currentDay.setDate(startOfWeek.getDate() + i);
     week.push(new Date(currentDay));
-    console.log(`${i} - ${week[i]}`);
+    // console.log(`${i} - ${week[i]}`);
   }
   return week;
 }
@@ -23,6 +23,13 @@ const formatDateToDDMMYYYY = (date: Date): string => {
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Месяцы начинаются с 0
   const year = date.getFullYear();
   return `${day}-${month}-${year}`;
+}
+
+const formatDateToStrUrl = (date: Date): string => {
+  const day = date.getDate();
+  const month = (date.getMonth() + 1); // Месяцы начинаются с 0
+  const year = String(date.getFullYear()).replace("20", "");
+  return `${day}_${month}_${year}`
 }
 
 const getDayFromDate = (date: string): string => {
@@ -107,13 +114,31 @@ const options = {
 };
 
 const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormatOptions) => {
+
+  const [apiUrl_schedule, setApiUrlSchedule] = useState<string|undefined>(process.env.URL_API_SHEDULE);
+  const [apiURL_auditorium, setApiUrlAuditorium] = useState<string|undefined>(process.env.URL_API_AUDITORIUM);
+  const [apiURL_university_unit, setApiUrlUniversityUnit] = useState<string|undefined>(process.env.URL_API_UNIVERSITYUNIT);
   const [schedule, setSchedule] = useState<Schedule | null>(null);
-  const apiUrl = process.env.URL_API_SHEDULE;
+  const [auditoriums, setAuditoriums] = useState<AuditoriumsInfo[]|null>(null);
+  const [allow_auditoriums, setAllowAuditoriums] = useState<AuditoriumsInfo[]|null>(null);
+  const [university_unit, setUniversityUnit] = useState<UniversityUnit[] |null>(null);
+
+
+  let defaultValueUniversityUnit = 1;
+  if (university_unit) {
+    defaultValueUniversityUnit = university_unit[0].id;
+  }
+  const [selectUniversityUnitStr, setSelectUniversityUnitStr] = useState<string|null>(String(defaultValueUniversityUnit));
+  // @ts-ignore
+  const [selectUniversityUnit, setSelectUniversityUnit] = useState<UniversityUnit|null>(university_unit?.find(el => el.id == defaultValueUniversityUnit));
+  const [week, setWeek] = useState<Date[]>(getWeekDates(new Date()));
+  const [dateCalendar, setDateCalendar] = useState<Date>(new Date());
+
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
         // @ts-ignore
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl_schedule);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -126,8 +151,8 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
 
     fetchData();
   }, []);
-  const [auditoriums, setAuditoriums] = useState<AuditoriumsInfo[]|null>(null);
-  const apiURL_auditorium = process.env.URL_API_AUDITORIUM;
+
+
   console.log(apiURL_auditorium);
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
@@ -141,6 +166,7 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
         const jsonData = await response.json();
         console.log(jsonData);
         setAuditoriums(jsonData as AuditoriumsInfo[]);
+        setAllowAuditoriums(jsonData as AuditoriumsInfo[])
       }
       catch (error) {
         console.error('Error fetching schedule:', error);
@@ -149,8 +175,6 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
     fetchData();
   }, []);
 
-  const [university_unit, setUniversityUnit] = useState<UniversityUnit[] |null>(null);
-  const apiURL_university_unit = process.env.URL_API_UNIVERSITYUNIT;
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
@@ -169,31 +193,64 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
     fetchData();
   }, []);
 
-  let defaultValueUniversityUnit = 1;
-  if (university_unit) {
-    defaultValueUniversityUnit = university_unit[0].id;
-  }
-  const [selectUniversityUnit, setSelectUniversityUnit] = useState<string|null>(String(defaultValueUniversityUnit));
   const handleSelectUniversityUnit = (event: any) => {
-    setSelectUniversityUnit(event.target.value);
+    setSelectUniversityUnitStr(event.target.value);
   }
 
   const hanldeViewButton = () => {
     if (!university_unit) return;
-    let unit = university_unit.find(el => el.id == Number(selectUniversityUnit));
+    let unit = university_unit.find(el => el.id == Number(selectUniversityUnitStr));
     if (!unit) {
-      console.error(`Error to get UniversityUnit object by id: ${selectUniversityUnit}`);
+      console.error(`Error to get UniversityUnit object by id: ${selectUniversityUnitStr}`);
       return;
     }
-    const week = getWeekDates(dateCalendar);
-    console.log(selectUniversityUnit);
+    //const week = getWeekDates(dateCalendar);
+    console.log(selectUniversityUnitStr);
     console.log(unit);
     console.log(dateCalendar);
-    console.log(week);
+    setSelectUniversityUnit(unit);
+    setWeek(getWeekDates(dateCalendar));
+    let auds: AuditoriumsInfo[] = []
+    auditoriums?.forEach((element) => {
+      if (element.university_unit == unit?.id) {
+        auds.push(element);
+      }
+    });
+    console.log("auds:", auds);
+    setAllowAuditoriums(auds);
+    // console.log(week);
+
+  // apiUrl_schedule
+  //   get-booking-week/1/23_10_23-29_10_23/
+    const getBooking = "get-booking-current-week";
+
+    // @ts-ignore
+    const setBooking = `get-booking-week/${unit.id}/${formatDateToStrUrl(week[0])}-${formatDateToStrUrl(week[6])}`;
+    console.log(setBooking);
+    const new_api = apiUrl_schedule?.replace(getBooking, setBooking);
+    console.log("new api:", new_api);
+    setApiUrlSchedule(new_api);
+
+    console.log("Api: ", apiUrl_schedule);
+    const fetchData = async (): Promise<void> => {
+      try {
+        // @ts-ignore
+        const response = await fetch(apiUrl_schedule);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setSchedule(jsonData as Schedule);
+        console.log("Schedule: ", schedule);
+      } catch (error) {
+        console.error('Error fetching schedule:', error);
+      }
+    };
+
+    fetchData();
 
   }
 
-  const [dateCalendar, setDateCalendar] = useState<Date>(new Date());
   const dateCalendarHandler = (element: any) => {
     try {
       const date = new Date(element.target.value);
@@ -207,20 +264,10 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
   }
 
   // @ts-ignore
-  const start_week = new Date(schedule?.start_week);
-  // @ts-ignore
-  const end_week = new Date(schedule?.end_week);
-  let week: Date[] = [];
-  for (let cur = start_week; cur<=end_week; cur.setDate(cur.getDate() + 1)) {
-    week.push(new Date(cur));
-    console.log(cur);
-  }
-  // @ts-ignore
   return (
     <div className={"container-xxl"}>
       <h2 className={"text-center"}>Расписание</h2>
-      {schedule && auditoriums && university_unit ?  (
-        <div>
+      {auditoriums && university_unit ? (
           <div className={"row align-items-center"}>
             <div className={"col"}>
               <select onChange={handleSelectUniversityUnit} onLoadedData={handleSelectUniversityUnit}>
@@ -241,10 +288,14 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
               <button className={"btn btn-success"} onClick={hanldeViewButton}>Показать</button>
             </div>
           </div>
+      ) : (<>Loading...</>)}
+      {schedule && auditoriums && university_unit ?  (
+        <div>
+
 
           <br/>
-          <p>Start Week: { start_week.toDateString()}</p>
-          <p>End Week: {end_week.toDateString()}</p>
+          <p>Start Week: { getWeekDates(dateCalendar)[0].toDateString()}</p>
+          <p>End Week: {getWeekDates(dateCalendar)[6].toDateString()}</p>
           <ul>
             {university_unit?.map((unit) => (
                 <li key={unit.id}>{unit.name}</li>
@@ -254,8 +305,13 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
             <thead>
               <tr>
                 <th className={"text-center"}>Дни недели</th>
-                {auditoriums.map((aud) => (
-                    <th key={aud.id} className={"text-center"}>{aud.name}</th>
+                {allow_auditoriums.map((aud) => (
+                    aud.university_unit == selectUniversityUnit?.id ? (
+                        <>
+                          <th key={aud.id} className={"text-center"}>{aud.name}</th>
+                        </>
+                      ) : (<></>)
+
                 ))}
               </tr>
             </thead>
@@ -264,14 +320,18 @@ const SchedulePage = (locales?: string | string[], options?: Intl.DateTimeFormat
                 <>
                 <tr>
                   <td>{day.toLocaleDateString("ru-RU", options)}</td>
-                  {auditoriums.map((aud) => (
+                  {allow_auditoriums.map((aud) => (
+                      // aud.university_unit == selectUniversityUnit?.id ? (
+                      //     <>
+                      //
+                      //     </>) : (<></>)
                       <>
                       <td>
                         <table className={"table table-bordered border-3"}>
                           {schedule.results.map((item) => (
                               <>
                               {
-                                item.auditorium[0].id == aud.id &&
+                                item.auditorium[0].id == aud.id  &&
                                 getDayFromDate(formatDateToDDMMYYYY(day)) == item.date.split("-")[0] &&
                                 getMonthFromDate(formatDateToDDMMYYYY(day)) == item.date.split("-")[1] &&
                                 getYearFromDate(formatDateToDDMMYYYY(day)) == item.date.split("-")[2] ? (
